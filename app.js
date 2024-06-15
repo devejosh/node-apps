29aed0ccd11a96a902ba051994202477c35c19e2
Devendra Joshi <devendra.joshi@outlook.com>
2024-06-16 02:36:04 +0530

    update/added yarns

--- app.js
+++
@@ -1,4 +0,0 @@
-// Importing the chalk lib to our project   
-import chalk from 'chalk';
-
-console.log (chalk.red.inverse("success!"))  
--- fs-module-test.js
+++
@@ -1,9 +0,0 @@
-// importing file system (fs) module.
-const fsModule = require ('fs')
-
-fsModule.writeFileSync('notes.txt', "My name is Devendra. This file was created with nodejs!") 
-
-// Appending data to the same file.
-// fs.appendFile(<file name /complete path to the file in question , 'data to append to the current file')
-
-fsModule.appendFileSync('notes.txt', 'This is the appended data to the file.')
