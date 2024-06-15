9a1a01a725e16d3ffc61f786ee5113b0ac66af87
Devendra Joshi <devendra.joshi@outlook.com>
2024-06-16 05:12:31 +0530

    fix/configured all routes

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,26 +1,36 @@
+//Core module 
+const path = require('path')
 // Importing express. 
 const express = require ('express')
 
 // Instanciate express 
 const server = express ()
 
-server.get('/', (req, res) => {
-    res.send("Welcome to express!")
+resPath = path.join (__dirname , '../resources/static') 
+
+//Tell the server to serve static files from the static directory
+server.use('/static', express.static(resPath))
+
+//Set routes
+server.get('/' , (req, res) => {
+    res.sendFile(path.join(resPath , 'html/index.html'))
 })
 
 server.get('/help', (req, res) => {
-    res.send("Help page!")
+    res.sendFile(path.join(resPath , 'html/help.html'))
 })
 
-server.get ('/about', (req, res) => {
-    res.send("Welcome to the about page.")
+server.get('/about', (req, res) => {
+    res.sendFile(path.join(resPath , 'html/about.html'))
 })
 
 server.get('/weather', (req, res) => {
-    res.send ("Welcome to the weather page!")
+    res.sendFile(path.join(resPath , 'html/weather.html'))
 })
 
 //Start the server 
 server.listen(3000, () => {
     console.log ("Server is running at port 3000")
 })
+
+console.log(resPath)
