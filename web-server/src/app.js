aa70d4942b60081842806e61c9cb75543e630d2e
Devendra Joshi <devendra.joshi@outlook.com>
2024-06-16 10:04:59 +0530

    fix/fixed routes. templating completed

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,30 +2,48 @@
 const path = require('path')
 // Importing express. 
 const express = require ('express')
+//Importing hbs
+const hbs = require('hbs')
 
 // Instanciate express 
 const server = express ()
 
 resPath = path.join (__dirname , '../resources/static') 
+const viewsPath = path.join (__dirname , '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+
+server.set('view engine','hbs')
+server.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 //Tell the server to serve static files from the static directory
 server.use('/static', express.static(resPath))
 
 //Set routes
-server.get('/' , (req, res) => {
-    res.sendFile(path.join(resPath , 'html/index.html'))
+
+server.get("/", (req, res) => {
+    res.render('index', {
+        'title' : "Home"
+    })
+})
+
+server.get ('/about' , (req, res) => {
+    res.render('about', {
+        'title' : 'About'
+    })
 })
 
 server.get('/help', (req, res) => {
-    res.sendFile(path.join(resPath , 'html/help.html'))
+    res.render('help', {
+        'title' : 'Help'
+    })
 })
 
-server.get('/about', (req, res) => {
-    res.sendFile(path.join(resPath , 'html/about.html'))
-})
-
-server.get('/weather', (req, res) => {
-    res.sendFile(path.join(resPath , 'html/weather.html'))
+server.get ('/weather', (req, res) => {
+    res.render('weather' , {
+        'title' : 'Weather'
+    })
 })
 
 //Start the server 
@@ -33,4 +51,3 @@ server.listen(3000, () => {
     console.log ("Server is running at port 3000")
 })
 
-console.log(resPath)
